38536d0db2461e2c15afb5400e1a92d923bd2b45
Mahangu <mahangu@automattic.com>
2020-07-15 08:47:38 +0530

    Moved songplay_table creation to end of list.

--- sql_queries.py
+++ sql_queries.py
@@ -26,7 +26,7 @@ staging_songs_table_create = ("""
 
 songplay_table_create = ("""
 CREATE TABLE songplays (
-songplay_id serial,
+songplay_id bigint IDENTITY(1, 1),
 start_time timestamp references time(start_time),
 user_id int references users(user_id),
 level text,
@@ -109,7 +109,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
