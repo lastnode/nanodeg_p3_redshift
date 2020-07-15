489bd47b14f95007d695a9542cb32378baf089dc
Mahangu <mahangu@automattic.com>
2020-07-15 08:34:35 +0530

    Copied in create table queries from Project 1.

--- sql_queries.py
+++ sql_queries.py
@@ -7,46 +7,77 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE IF EXISTS \"staging_events_table\""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events_table"
 staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
-DROP TABLE IF EXISTS "staging_events_table";
-CREATE TABLE "staging_events_table" (
-    "id" double precision DEFAULT nextval('sporting_event_ticket_seq') NOT NULL,
-    "sporting_event_id" double precision NOT NULL,
-    "sport_location_id" double precision NOT NULL,
-    "seat_level" numeric(1,0) NOT NULL,
-    "seat_section" character varying(15) NOT NULL,
-    "seat_row" character varying(10) NOT NULL,
-    "seat" character varying(10) NOT NULL,
-    "ticketholder_id" double precision,
-    "ticket_price" numeric(8,2) NOT NULL);
+staging_events_table_create = ("""
+
 """)
 
 staging_songs_table_create = ("""
 """)
 
 songplay_table_create = ("""
+CREATE TABLE songplays (
+songplay_id serial,
+start_time timestamp references time(start_time),
+user_id int references users(user_id),
+level text,
+song_id text references songs(song_id),
+artist_id text references artists(artist_id),
+session_id int,
+location text,
+user_agent text,
+PRIMARY KEY (songplay_id))
 """)
 
 user_table_create = ("""
+CREATE TABLE users (
+user_id int,
+first_name text,
+last_name text,
+gender text,
+level text,
+PRIMARY KEY (user_id))
 """)
 
 song_table_create = ("""
+CREATE TABLE songs (
+song_id text,
+title text,
+artist_id text,
+year int,
+duration double precision,
+PRIMARY KEY (song_id))
 """)
 
 artist_table_create = ("""
+CREATE TABLE artists (
+artist_id text,
+name text,
+location text,
+latitude double precision,
+longitude double precision,
+PRIMARY KEY (artist_id))
 """)
 
 time_table_create = ("""
+CREATE TABLE time (
+start_time timestamp,
+hour int,
+day int,
+week_of_year int,
+month int,
+year int,
+weekday int,
+PRIMARY KEY (start_time))
 """)
 
 # STAGING TABLES
@@ -78,10 +109,10 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create]
-drop_table_queries = [staging_events_table_drop]
-
-# create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-# drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
+# staging_events_table_drop, staging_songs_table_drop
+# staging_events_table_create, staging_songs_table_create,
